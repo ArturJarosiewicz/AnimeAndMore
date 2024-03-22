ef105ba7327f9d0fde0c5b9c6bf81d469a454cfe
Artur <artur.jarosiewicz6@gmail.com>
2024-03-22 23:11:11 +0100

    Add data file and images. Display it on page

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import pandas as pd
+
+
+df = pd.read_csv('data.csv', sep=';')
+
+st.set_page_config(page_title='Hokuto no Ken')
 
 col1, col2 = st.columns(2)
 
@@ -13,3 +19,11 @@ with col2:
     successors in its 1,800 year history.
     """
     st.info(content)
+
+st.subheader('If You are a fan you should know below characters')
+
+
+for index, row in df.iterrows():
+    st.subheader(row['name'])
+    st.info(row['description'])
+    st.image(f"images/{row['image']}")
