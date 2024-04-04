fbbd17ad3b46254c72ea1b6f73dfe638e8861c07
Artur <artur.jarosiewicz6@gmail.com>
2024-04-04 23:00:02 +0200

    Add Baki page and fill it with content

---
+++ pages/Baki Grappler.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+st.set_page_config(page_title="Baki Grappler", layout="wide")
+
+st.header("Baki Grappler (2001)")
+
+content1 = """
+I'm still wathing this series so maybe there will be an update in future. For now i really like that story. It is
+focused on young(13 y.o!) boy named Baki Hanma who wanna be the strongest, most skilled fighter on earth.
+Interesting thing in this story is that main antagonist is Baki's father Yujiro Hanama. Owner of the best physice of 
+all anime i've ever seen, specially the back muscles
+"""
+
+content2 = """
+Plot is simple but it dont boder me. We can find here old scheme from zero to hero.
+The center of anime is fighting which is really cool in it. Baki defeats increasingly stronger opponents on his way.
+My fav for now is huge, silent and incedibly strong yakuza mob Hanayama.
+"""
+
+content3 = """
+If you like action fighting anime like dragon ball you have to watch Baki. 
+"""
+
+st.image("images/baki/1.png")
+st.text(content1)
+st.image("images/baki/2.png")
+st.image("images/baki/3.png")
+st.text(content2)
+st.image("images/baki/4.png")
+st.text(content3)
